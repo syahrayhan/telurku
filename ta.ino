f23a98753d53a2c6f1b6a3267b76d7c3496c6b5b
syahrayhan <syahrayhanid@gmail.com>
2021-04-02 22:23:34 +0700

    bip bip horray :) store data to eeprom

--- ta.ino
+++ ta.ino
@@ -1,32 +1,33 @@
 #include <ESP8266WiFi.h>
 #include <FirebaseESP8266.h>
-#include <DHT.h>  
-
+#include <DHT.h>
+#include <EEPROM.h>
 //3 jam 10800000
 const unsigned long eventInterval = 60000;
 unsigned long previousTime = 0;
-const char* ssid = "Redmi9a";
-const char* password = "123456789101";
+const char *ssid = "Redmi9a";
+const char *password = "123456789101";
 
 long elapsedMillis;
 long startMillis = millis();
 
-float temp;
-int a;
-int b;
-int c;
-int d;
+float temp, valETemp;
+int addrsTemp, valEReset, valEShutdown;
+int addrsShutdown, addrsReset, dataShutdown, dataReset;
 
-#define relay 5         // Untuk D1     penggerak 
-#define relay2 4        // Untuk D2     3 lampu
-#define dht_pin 2      // Untuk D4      dht
+int addrsETemp = 0;
+int addrsEShutdown = 1;
+int addrsEReset = 2;
+
+#define relay 5   // Untuk D1     penggerak
+#define relay2 4  // Untuk D2     3 lampu
+#define dht_pin 2 // Untuk D4      dht
 
 FirebaseData firebaseData;
 DHT dht(dht_pin, DHT11);
 
-
-
-void setup() {
+void setup()
+{
     Serial.begin(9600);
     pinMode(relay, OUTPUT);
     pinMode(relay2, OUTPUT);
@@ -36,42 +37,69 @@ void setup() {
     connectWifi();
     Firebase.begin("eggcubator-d446f-default-rtdb.firebaseio.com", "4KYOs3lhfrbQdWKqAu9P5JkMgTtn6nwUV1WqC07i");
     dht.begin();
+    EEPROM.begin(512);
+    saveeeprom();
 }
 
-void offline() {
-   
+void saveeeprom()
+{
+    addrsTemp = Firebase.getInt(firebaseData, "/EggData/maxTemp");
+    temp = firebaseData.intData();
+    
+    EEPROM.write(addrsETemp, temp);
+    Serial.println("Berhasil Write ETemp");
+
+    addrsShutdown = Firebase.getInt(firebaseData, "/SettingData/isShutdown");
+    dataShutdown = firebaseData.intData();
+    addrsReset = Firebase.getInt(firebaseData, "/SettingData/isReset");
+    dataReset = firebaseData.intData();
+
+    EEPROM.write(addrsEReset, dataReset);
+    Serial.println("Berhasil Write EReset");
+
+    EEPROM.write(addrsEShutdown, dataShutdown);
+    Serial.println("Berhasil Write EShutdown");
 }
 
-void connectWifi() {
+void connectWifi()
+{
     WiFi.begin(ssid, password);
 
-    while(WiFi.status() != WL_CONNECTED) {
+    while (WiFi.status() != WL_CONNECTED)
+    {
         delay(500);
         Serial.print(".");
-    } 
+    }
     Serial.println(WiFi.status());
 
-      if(WiFi.status() != WL_CONNECT_FAILED){
-    Serial.println("online");
-    a = Firebase.getInt(firebaseData, "/SettingData/isShutdown");
-    b = firebaseData.intData();
-    c = Firebase.getInt(firebaseData, "/SettingData/isReset");
-    d = firebaseData.intData();
-  }
     Serial.println("Sukses terkoneksi wifi!");
     Serial.println("IP Address: ");
     Serial.println(WiFi.localIP());
 }
 
-
-void sensorDHT() {
+void sensorDHT()
+{
     float kelembaban = dht.readHumidity();
     float suhu = dht.readTemperature();
 
-    if(isnan(kelembaban) || isnan(suhu)) {
+    if (isnan(kelembaban) || isnan(suhu))
+    {
         Serial.println("Gagal untuk membaca sensor DHT :(");
         return;
     }
+
+    addrsTemp = Firebase.getInt(firebaseData, "/EggData/maxTemp");
+    temp = firebaseData.intData();
+
+    valETemp = EEPROM.read(addrsETemp);
+    if (valETemp != temp)
+    {
+        EEPROM.write(addrsETemp, temp);
+        Serial.println("Berhasil Write ETemp");
+    }
+
+    Serial.print("valETemp = ");
+    Serial.println(valETemp);
 
     Serial.print("Kelembaban: ");
     Serial.print(kelembaban);
@@ -80,129 +108,173 @@ void sensorDHT() {
     Serial.print(suhu);
     Serial.println();
 
-    if (Firebase.setFloat(firebaseData, "/FirebaseIOT/temperature", suhu)) {
+    if (Firebase.setFloat(firebaseData, "/FirebaseIOT/temperature", suhu))
+    {
         Serial.println("Data suhu terkirim :)");
         // Serial.println();
-    } else {
+    }
+    else
+    {
         Serial.println("Data suhu gagal terkirim :(");
         Serial.println("Error : " + firebaseData.errorReason());
         Serial.println();
     }
 
-    if (Firebase.setFloat(firebaseData, "/FirebaseIOT/humidity", kelembaban)) {
+    if (Firebase.setFloat(firebaseData, "/FirebaseIOT/humidity", kelembaban))
+    {
         Serial.println("Data kelembaban terkirim :)");
         Serial.println();
-    } else {
+    }
+    else
+    {
         Serial.println("Data kelembaban gagal terkirim :(");
         Serial.println("Error : " + firebaseData.errorReason());
         Serial.println();
     }
 
-
-   if (Firebase.getInt(firebaseData, "/EggData/maxTemp")) {
-       temp = firebaseData.intData();
-       if (suhu > temp) {
-           Serial.println("Lampu mati");
-           Serial.println(temp);
-           digitalWrite(relay2, HIGH);
-       } else if (suhu < (temp - 1.5)) {
-           Serial.println("Lampu hidup");
-           digitalWrite(relay2, LOW);
-       }
-   } else {
-       Serial.print("Error saat ambil data, Menggunakan data awal ");
-       Serial.println(temp);
-       Serial.println(firebaseData.errorReason());
-        if (suhu > temp) {
-           Serial.println("Lampu mati");
-           Serial.println(temp);
-           digitalWrite(relay2, HIGH);
-       } else if (suhu < (temp - 1.5)) {
-           Serial.println("Lampu hidup");
-           digitalWrite(relay2, LOW);
-       }
-   }
+    if (addrsTemp)
+    {
+        if (suhu > temp)
+        {
+            Serial.println("Lampu mati");
+            Serial.println(temp);
+            digitalWrite(relay2, HIGH);
+        }
+        else if (suhu < (temp - 1.5))
+        {
+            Serial.println("Lampu hidup");
+            digitalWrite(relay2, LOW);
+        }
+    }
+    else
+    {
+        Serial.print("Error saat ambil data, Menggunakan data awal ");
+        Serial.println(valETemp);
+        Serial.println(firebaseData.errorReason());
+        if (suhu > valETemp)
+        {
+            Serial.println("Lampu mati");
+            Serial.println(valETemp);
+            digitalWrite(relay2, HIGH);
+        }
+        else if (suhu < (valETemp - 1.5))
+        {
+            Serial.println("Lampu hidup");
+            digitalWrite(relay2, LOW);
+        }
+    }
 }
 
+void loop()
+{
+    unsigned long currentTime = millis();
 
-void loop () {
-  unsigned long currentTime = millis();
+    if (WiFi.status() != WL_CONNECT_FAILED)
+    {
+        Serial.println("online");
+        addrsShutdown = Firebase.getInt(firebaseData, "/SettingData/isShutdown");
+        dataShutdown = firebaseData.intData();
+        addrsReset = Firebase.getInt(firebaseData, "/SettingData/isReset");
+        dataReset = firebaseData.intData();
 
-  if(WiFi.status() != WL_CONNECT_FAILED){
-    Serial.println("online");
-    a = Firebase.getInt(firebaseData, "/SettingData/isShutdown");
-    b = firebaseData.intData();
-    c = Firebase.getInt(firebaseData, "/SettingData/isReset");
-    d = firebaseData.intData();
-  }
-  Serial.println("not online");
-  Serial.print("data dari firebase ");
-  Serial.println(a);
-  Serial.print("ini yang baru");
-  Serial.println(b);
+        valEReset = EEPROM.read(addrsEReset);
+        valEShutdown = EEPROM.read(addrsEShutdown);
+        if (valEReset != dataReset)
+        {
+            EEPROM.write(addrsEReset, dataReset);
+            Serial.println("Berhasil Write EReset");
+        }
 
-  Serial.print("data dari reset ");
-  Serial.println(c);
-  Serial.print("ini yang baru reset");
-  Serial.println(d);
+        if (valEShutdown != dataShutdown)
+        {
+            EEPROM.write(addrsEShutdown, dataShutdown);
+            Serial.println("Berhasil Write EShutdown");
+        }
+    }
 
-  if(a) {
-    if(b == 0 && d == 0) {
-        // Serial.println("alat hidup");
-        sensorDHT();
-        if(c) {
-            if (d == 0) {
-                // Serial.println("alat hidup dan tidak direset");
-                if (currentTime - previousTime >= eventInterval) {
-                    /* Event code */ 
-                    Serial.println("Ice Ice Baby");
-                    digitalWrite(relay, LOW);
-                    delay(8000);
-                    Serial.println("nom nom");
-                    digitalWrite(relay, HIGH);
-            
-                /* Update the timing for the next time around */
+    Serial.print("addrs Shutdown =  ");
+    Serial.println(addrsShutdown);
+    Serial.print("data Shutdown = ");
+    Serial.println(dataShutdown);
+
+    Serial.print("addrs Reset = ");
+    Serial.println(addrsReset);
+    Serial.print("data Reset = ");
+    Serial.println(dataReset);
+
+    Serial.print("valEReset = ");
+    Serial.println(valEReset);
+    Serial.print("valEShutdown = ");
+    Serial.println(valEShutdown);
+
+    if (addrsShutdown)
+    {
+        if (dataShutdown == 0 && dataReset == 0)
+        {
+            sensorDHT();
+            if (addrsReset)
+            {
+                if (dataReset == 0)
+                {
+                    if (currentTime - previousTime >= eventInterval)
+                    {
+                        /* Event code */
+                        Serial.println("Ice Ice Baby");
+                        digitalWrite(relay, LOW);
+                        delay(7000);
+                        Serial.println("nom nom");
+                        digitalWrite(relay, HIGH);
+
+                        /* Update the timing for the next time around */
+                        previousTime = currentTime;
+                    }
+                }
+                else
+                {
+                    // Serial.println("alat hidup dan tidak direset");
                     previousTime = currentTime;
                 }
-            } else {
-                // Serial.println("alat hidup dan tidak direset");
+            }
+        }
+        else
+        {
+            Serial.println("alat mati");
+            digitalWrite(relay, HIGH);
+            digitalWrite(relay2, HIGH);
+            digitalWrite(dht_pin, HIGH);
+        }
+    }
+    else
+    {
+        if (valEShutdown == 0 && valEReset == 0)
+        {
+            sensorDHT();
+            if (valEReset == 0)
+            {
+                if (currentTime - previousTime >= eventInterval)
+                {
+                    /* Event code */
+                    Serial.println("Dinamo berputar");
+                    digitalWrite(relay, LOW);
+                    delay(7000);
+                    digitalWrite(relay, HIGH);
+
+                    /* Update the timing for the next time around */
+                    previousTime = currentTime;
+                }
+            }
+            else
+            {
                 previousTime = currentTime;
             }
         }
-    } else {
-        Serial.println("alat mati");
-        digitalWrite(relay, HIGH);
-        digitalWrite(relay2, HIGH);
-        digitalWrite(dht_pin, HIGH);
+        else
+        {
+            Serial.println("alat mati");
+            digitalWrite(relay, HIGH);
+            digitalWrite(relay2, HIGH);
+            digitalWrite(dht_pin, HIGH);
+        }
     }
-  } else {
-  if(b == 0 && d == 0) {
-        Serial.println("alat hidup");
-        sensorDHT();
-            if (d == 0) {
-                 Serial.println("alat hidup dan tidak direset");
-                if (currentTime - previousTime >= eventInterval) {
-                    /* Event code */
-                    Serial.println("Ice Ice Baby");
-                    digitalWrite(relay, LOW);            
-                    delay(8000);
-                    Serial.println("nom nom");
-                    digitalWrite(relay, HIGH);
-            
-                /* Update the timing for the next time around */
-                    previousTime = currentTime;
-                }
-            } else {
-                // Serial.println("alat hidup dan tidak direset");
-                previousTime = currentTime;
-            }
-    } else {
-        Serial.println("alat mati");
-        digitalWrite(relay, HIGH);
-        digitalWrite(relay2, HIGH);
-        digitalWrite(dht_pin, HIGH);
-    }
-  
-  }
     delay(250);
 }
